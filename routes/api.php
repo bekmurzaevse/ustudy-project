a52f0c515cc7396e2e19d6e2fc8b4daeff76526f
bekmurzaevse <bekmurzaevse@gmail.com>
2024-12-04 21:13:49 +0500

    Redis Cache

--- routes/api.php
+++ routes/api.php
@@ -1,61 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PostController;
+
+
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cache;
 
 
-Route::get("me", function () {
-    return response()->json([
-        "data" => "DATA"
-    ]);
-});
-
-Route::get("params", function (Request $request) {
-    return response()->json([
-        "data"=> $request->all()
-    ]);
-});
-
-Route::get("path/{id}", function ($id) {
-    return response()->json([
-        "id" => $id
-    ]);
-});
-
-Route::post("store", function (Request $request) {
-    return response()->json([
-        "data"=> $request->all()
-    ]);
-});
-
-Route::post("login", function (Request $request) {
-    return response()->json([
-        "data"=> $request->login
-    ]);
-});
-
-Route::match(['put', 'patch'], 'update', function () {
-    return response()->json([
-        'data'=> "Put or PATCH"
-    ]);
-});
-
-
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-// Route::get("info", function () {
-//     return response()->json([
-//         "message"=> "laravel",
-//         "success" => true,
-//         "data" => [
-//             "bir" => 1,
-//             "eki" => 2,
-//         ],
-//     ]);
-// });
+Route::get("posts", [PostController::class,"posts"]);
 
 
 
